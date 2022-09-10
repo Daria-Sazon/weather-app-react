9ad6516eeafa7e8123c0803f66764c7aac34d23e
Daria <daria.sazonenko12@gmail.com>
2022-09-10 16:14:23 +0300

    added final version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import "./App.css";
+import React from "react";
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather defaultCity="Kyiv" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Daria-Sazon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Daria Sazon
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Daria-Sazon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="3"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
